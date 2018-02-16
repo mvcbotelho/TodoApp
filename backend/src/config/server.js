d987401337019db38ca6b07194d9a675d7ec18f2
mvcbotelho <marcus.itec@gmail.com>
2018-02-16 18:53:42 -0300

    Add cors in app

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const server = express()
+const allowCors = require('./cors')
 
 const port = 3003
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function(){
   console.log(`BACKEND is running on port ${port}`) 
