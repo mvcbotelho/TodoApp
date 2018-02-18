ae500908c741e2e3b252dea1bbabc3e44ebcc313
mvcbotelho <marcus.itec@gmail.com>
2018-02-17 23:24:51 -0300

    Create page header end implement in About and Todo

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name='Sobre' small='nós' />
+
+    <h2>Nossa Histório</h2>
+    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica</p>
+    <h2>Missão e Visão</h2>
+    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica</p>
+    <h2>Imprensa</h2>
+    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica</p>
   </div>
 ) 
